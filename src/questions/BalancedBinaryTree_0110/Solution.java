5299de0c056209c4808a994fca54b86130695f69
wangjojo <weiainijiujiu@126.com>
2020-06-18 17:43:56 +0800

    110 添加自底向上的方法

--- src/questions/BalancedBinaryTree_0110/Solution.java
+++ src/questions/BalancedBinaryTree_0110/Solution.java
@@ -63,11 +63,11 @@ import questions.common.TreeNode;
  */
 class Solution {
     /*
-        这个方法有问题，题目要求子树也要满足平衡的条件
-     * 使用BST，层次遍历，找出第一个left/right节点为null的节点，记录起所在的层，再找出最后一层的的层数
-     * 
+     * 使用BST，层次遍历，找出第一个left/right节点为null的节点，记录起所在的层，再找出最后一层的的层数(这个方法有问题，
+     * 题目要求子树也要满足平衡的条件)
+     *
      */
-    public boolean isBalanced(TreeNode root) {
+    public boolean isBalanced2(TreeNode root) {
         if (root == null) return true;
         return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
     }
@@ -76,6 +76,39 @@ class Solution {
             return 0;
         }
         return Math.max(height(node.left) , height(node.right)) + 1;
+    }
+    // 如果一个树是平衡二叉树,那么他是否一定是完全二叉树呢?
+    /*
+    */
+    final class TreeInfo {
+        public final int height;
+        public final boolean balanced;
+
+        public TreeInfo(int height, boolean balanced) {
+            this.height = height;
+            this.balanced = balanced;
+        }
+    }
+    public boolean isBalanced(TreeNode root) {
+        return isBalancedTreeHelper(root).balanced;
+    }
+    private TreeInfo isBalancedTreeHelper(TreeNode node) {
+        if (node == null) {
+            return new TreeInfo(-1, true);
+        }
+        TreeInfo left = isBalancedTreeHelper(node.left);
+        if (!left.balanced) {
+            return new TreeInfo(-1, false);
+        }
+        TreeInfo right = isBalancedTreeHelper(node.right);
+        if (!right.balanced) {
+            return new TreeInfo(-1, false);
+        }
+        if (Math.abs(left.height - right.height) < 2) {
+            return new TreeInfo(Math.max(left.height, right.height) + 1, true);
+        } else {
+            return new TreeInfo(-1, false);
+        }
     }
 }
 // @lc code=end
