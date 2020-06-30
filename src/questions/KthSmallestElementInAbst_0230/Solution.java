b000ff6feb509fdb493c89a2a51037a843d211b9
wangjojo <weiainijiujiu@126.com>
2020-06-30 11:28:09 +0800

    [230] 二叉搜索树中第K小的元素 使用自定义堆栈

---
+++ src/questions/KthSmallestElementInAbst_0230/Solution.java
@@ -0,0 +1,84 @@
+package questions.KthSmallestElementInAbst_0230;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+import questions.common.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=230 lang=java
+ *
+ * [230] 二叉搜索树中第K小的元素
+ *
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (70.51%)
+ * Likes:    231
+ * Dislikes: 0
+ * Total Accepted:    56.8K
+ * Total Submissions: 80.3K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ * 
+ * 说明：
+ * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * 输出: 1
+ * 
+ * 示例 2:
+ * 
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * 输出: 3
+ * 
+ * 进阶：
+ * 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        inorderLeft(root, stack);
+        int i = 0;
+        while(i < k - 1) {
+            TreeNode n = stack.pop();
+            if (n.right != null) {
+                inorderLeft(n.right, stack);
+            }
+            i++;
+        }
+        return stack.pop().val;
+    }
+    private void inorderLeft(TreeNode node, Deque<TreeNode> stack) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+}
+// @lc code=end
